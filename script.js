525effed061150e5efc01d216335b14ae733d553
Prateek Verghese <prateek@2Prateeks-MacBook-Pro.local>
2022-09-19 23:38:10 -0700

    secondcommit

---
+++ script.js
@@ -0,0 +1,14 @@
+var startButton = document.getElementsByClassName("stsrt-button")
+var formWrap = document.getElementById('question')
+
+
+
+function startGame(){
+
+}
+function setNextQuestion(){
+
+}
+function selectAnswer(){
+
+}
