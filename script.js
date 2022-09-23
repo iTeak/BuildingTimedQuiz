e5bc9e99c05f870bcdee6480810aafb59eb89e0e
Prateek Verghese <prateek@2Prateeks-MacBook-Pro.local>
2022-09-22 17:26:27 -0700

    added question shuffle and radomness

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var startButton = document.getElementById('startButton')
-var formWrap = document.getElementById('question')
 var NextButton = document.getElementById('NextButton')
-
 var choice1 = document.getElementsByClassName ('choice1')
 var choice2 = document.getElementsByClassName ('choice2')
 var choice3 = document.getElementsByClassName ('choice3')
 var choice4 = document.getElementsByClassName ('choice4')
-
+var showQuestion = document.getElementById('question')
+var showChoices = document.getElementById('choices')
+var sikeButton = document.getElementById('sike')
+let shuffledQuestions, currentQuestionIndex
 // var timer = document.getElementById('timer')
 
 
@@ -17,10 +18,11 @@ var choice4 = document.getElementsByClassName ('choice4')
 
 startButton.addEventListener('click' , startGame)
 NextButton.addEventListener('click' , populatequestion)
+sikeButton.addEventListener('click', motivation )
  
 
-const questions = [{ 
-    question1: "What does HTML standy for?",
+var questions = [{ 
+    question: "What does HTML standy for?",
     answers: [
         {text: 'Hyper Text Mark up Language', correct: true},
         {text:'Java Script', correct: false},
@@ -49,17 +51,27 @@ const questions = [{
 
 
 function startGame() 
-{ document.getElementById("title").innerHTML=""
+{document.getElementById("title").innerHTML=""
 startButton.classList.add('hide');
 NextButton.classList.remove('hide')
-
-  
+showChoices.classList.remove('hide')
+showQuestion.classList.remove('hide')
+sikeButton.classList.add('hide')
+document.getElementById('question').innerHTML= (questions[0].question);
+document.getElementById('choice1').innerHTML=(questions[0].answers[0].text);
+document.getElementById('choice2').innerHTML=(questions[0].answers[1].text);
+document.getElementById('choice3').innerHTML=(questions[0].answers[2].text);
+document.getElementById('choice4').innerHTML=(questions[0].answers[3].text);
+console.log(questions[0].answers[1])
 }
 
 function populatequestion(){
+    shuffledQuestions = questions.sort(()=> Math.random() - .5)
+    currentQuestionIndex=0
+    document.getElementById('question').innerHTML= (questions[0].question);
+    document.getElementById('question').innerHTML= (questions[1].question);
+    document.getElementById('question').innerHTML= (questions[2].question);
     console.log("working")
-document.getElementById('question').innerHTML= (questions[0].question1)
-document.getElementsByClassName('choice1').innerHTML=(questions[0].answers[0])
 
 
 
@@ -85,4 +97,8 @@ function selectAnswer(){
 
 function setNextQuestion(){
 
+}
+
+function motivation(){
+    alert("You thought")
 }
